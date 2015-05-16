d923180cba87f6e63f73e88f4769efe525f3e3c4
xuangong <zhangxian1124@gmail.com>
2015-05-17 01:57:47 +0800

    accomplish string class

--- string/main.cc
+++ string/main.cc
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <assert.h>
 #include "qh_string.h"
 
 
@@ -7,6 +9,22 @@ int main(int argc, char* argv[])
 {
     //TODO 在这里添加单元测试，越多越好，代码路径覆盖率越全越好
     //TODO 单元测试写法请参考INIParser那个项目，不要写一堆printf，要用assert进行断言判断。
+    qh::string str1;
+    assert(str1.data() == NULL && str1.size() == 0);
+
+    const char *s = "Hello world!";
+    qh::string str2(s);
+    assert(str2.size() == strlen(s));
+
+    char *bracket = str2[3];
+    assert(*bracket == 'l');
+
+    bracket = str2[20];
+    assert(bracket == NULL);
+
+    qh::string str3(s, 5);
+    assert(strlen(str3.c_str()) == 5);
+
 
 #ifdef WIN32
     system("pause");
--- string/qh_string.cc
+++ string/qh_string.cc
@@ -1,6 +1,8 @@
 #include "qh_string.h"
 
 #include <string.h>
+#include <cstring>
+#include <stdio.h>
 
 namespace qh
 {
@@ -13,25 +15,63 @@ namespace qh
 
     string::string( const char* s )
     {
+        len_ = strlen(s);
+        data_ = new char[len_ + 1];
+        if( data_ )
+        {
+            memcpy(data_, s, len_);
+            data_[len_] = '\0';
+        }
+        else
+            len_ = 0;
     }
 
     string::string( const char* s, size_t len )
     {
+        size_t sourceLen = strlen(s);
+        len_ = len > sourceLen ? sourceLen : len;
+
+        data_ = new char[len_ + 1];
+        if( data_ )
+        {
+            memcpy(data_, s, len_);
+            data_[len_] = '\0';
+        }
+        else
+            len_ = 0;
     }
 
     string::string( const string& rhs )
     {
-
+        if(rhs.data_ == NULL)
+        {
+            string();
+            return;
+        }
+        len_ = rhs.len_;
+        data_ = new char[len_ + 1];
+        if( data_ )
+        {
+            memcpy(data_, rhs.data_, len_);
+            data_[len_] = '\0';
+        }
+        else
+            len_ = 0;
     }
 
     string& string::operator=( const string& rhs )
     {
+        if(&rhs != this)
+        {
+            string(rhs);
+        }
         return *this;
     }
 
     string::~string()
     {
-
+        delete []data_;
+        len_ = 0;
     }
 
     size_t string::size() const
@@ -41,16 +81,20 @@ namespace qh
 
     const char* string::data() const
     {
-        return NULL;
+        return data_;
     }
 
     const char* string::c_str() const
     {
-        return NULL;
+        return data_;
     }
 
     char* string::operator[]( size_t index )
     {
+        if(index <= len_)
+        {
+            return data_ + index;
+        }
         return NULL;
     }
 }
